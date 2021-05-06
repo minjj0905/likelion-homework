021814bd142c1be320e8baef577651bde265c67c
minjj0905 <minjj0905@kookmin.ac.kr>
2021-05-06 21:44:59 +0900

    update wordcount session hw

---
+++ django_word_counter/django_word_counter/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('word_counter.urls')),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
